5eb97651ff1c564bfa545ac0b07a25f31ca40c5a
paca <frpacareu87@gmail.com>
2020-05-04 12:24:49 -0700

    object list and radio buttons

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-//let path = "/api/location/search/?query=";
 
-let city = "london";
-let root = `/api/location/search/?query=${city}`;
-let path = root;
-console.log(path);
-let key = "1769e6366be36237e61944ffeec72d70";
-
-//let path = "/api/location/search/?query=";
+// let key = "1769e6366be36237e61944ffeec72d70";
+let myList = [];
+const numOfReports = 7;
+let weatherDisplay = document.querySelector("#weatherDisplay");
 
 //let p = fetch('http://api.openweathermap.org/data/2.5/forecast?q=London&appid=41e2a23f88602f4cdbe27277677d0639');
 let p = fetch("weather-app.json");
-
 p.then(function (response) {
   if (response.status !== 200) {
     console.log(
@@ -21,44 +16,82 @@ p.then(function (response) {
   // Examine the text in the response
   response.json().then(function (data) {
     console.log(data.list);
-    drawList(data.list);
+    createMyList(data.list);
+    drawList();
+    console.log(myList);
   });
 }).catch(function (err) {
   console.log("Fetch Error :-S", err);
 });
 //°C
 //°F
-function drawList(list){
-    let weatherDisplay = document.querySelector("#weatherDisplay");
-    
-    for (let i = 0; i < 7; i++) {
-        const element = list[i];
-        let ulElement = document.createElement("ul");
-        let icon = document.createElement("img");
-        let liTemp = document.createElement("li");
-        let liHum = document.createElement("li");
-        let liMain = document.createElement("li");
-        icon.src = giveMeIcon(element.weather[0].icon);
-        liTemp.innerHTML = converTemp(element.main.temp);
-        liHum.innerHTML = `<li>Hum: ${element.main.humidity}%</li>`;
-        liMain. innerHTML = `<li>${element.weather[0].main}</li>`;
-        ulElement.appendChild(icon);
-        ulElement.appendChild(liTemp);
-        ulElement.appendChild(liHum);   
-        ulElement.appendChild(liMain);
-        //weatherDisplay.appendChild(icon);      
-        weatherDisplay.appendChild(ulElement);        
-    }
+function createMyList(list) {
+  for (let i = 0; i < numOfReports; i++) {
+    const element = list[i];
+    let tempOriginal = element.main.temp / 10;
+    tempOriginal = tempOriginal.toFixed();
+    let myTemperature = setTemperature(tempOriginal, giveMeUnit());
+    let forecast = {
+      icon: getIconURL(element.weather[0].icon),
+      temp: myTemperature,
+      unit: giveMeUnit(),
+      humi: element.main.humidity,
+      main: element.weather[0].main,
+    };
+    myList.push(forecast);
+  }
+}
+//Agarrar la temp inicial q viene en °C y
+//la almacena en la unidad correcta
+//sobre la base del radiobutton seleccionado
+function setTemperature(temp, unit) {
+  return unit === "F" ? (temp * 9) / 5 + 32 : temp;
+}
+function drawList() {  
+    weatherDisplay.innerHTML = "";
+  for (let i = 0; i < myList.length; i++) {
+    const element = myList[i];
+    let ulElement = document.createElement("ul");
+    let icon = document.createElement("img");
+    let liTemp = document.createElement("li");
+    let liHum = document.createElement("li");
+    let liMain = document.createElement("li");
+    icon.src = element.icon;
+    liTemp.innerHTML = `Temp: ${element.temp}${element.unit}`;
+    liHum.innerHTML = `Hum: ${element.humi}%`;
+    liMain.innerHTML = `${element.main}`;
+    ulElement.appendChild(icon);
+    ulElement.appendChild(liTemp);
+    ulElement.appendChild(liHum);
+    ulElement.appendChild(liMain);
+    weatherDisplay.appendChild(ulElement);
+  }
 }
 
-function converTemp(element){
-    let temp = parseInt(element)/10;
-    let radioF = document.querySelector("#F");
-    let unit = radioF.checked ? "°F" : "°C";
-    let result = `<li>Temp: ${temp}${unit}</li>`;
-    return result;    
+function giveMeUnit() {
+  let radioF = document.querySelector("#F");
+  return radioF.checked ? "°F" : "°C";
 }
-function giveMeIcon(iconName){
-    return ` http://openweathermap.org/img/wn/${iconName}@2x.png`;
+function getIconURL(iconName) {
+  return ` http://openweathermap.org/img/wn/${iconName}@2x.png`;
 }
-//console.log('weqwe');
+
+function converTemp(unit) {
+  for (let i = 0; i < myList.length; i++) {
+    const element = myList[i];
+    element.unit = unit;
+    if (unit === "°F") {
+        element.temp = ((element.temp * 9) / 5 + 32).toFixed();
+    }
+    else{
+        element.temp = ((element.temp - 32) * 5/9).toFixed();
+    }
+  }
+}
+function handleClick(myRadio) {
+  if (myRadio.value !== myList[0].unit) {
+    converTemp(myRadio.value);
+  }
+  drawList();
+}
+
