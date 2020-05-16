ed4f3268cf0b633d18e7c5a46b0fc621a460f9f0
paca <frpacareu87@gmail.com>
2020-05-15 21:50:01 -0700

    last design, final review

--- main.js
+++ main.js
@@ -3,13 +3,15 @@
 let key = "1769e6366be36237e61944ffeec72d70";
 let myList = [];
 let myCity = "";
+let myCountry = "";
 const numOfReports = 7;
 const submitButton = document.querySelector("#submit");
 const textBoxCity = document.querySelector("#city");
 let weatherDisplay = document.querySelector("#weatherDisplay");
 let radioBCelsius = document.querySelector("#Celcius");
 let radioBfahrenheit = document.querySelector("#Fahrenheit");
-let cityNameDisplay = document.querySelector("#city-name");
+let unitDIV = document.querySelector("#unitDIV");
+let mainReport = document.querySelector("#mainReport");
 
 //Agarrar la temp inicial q viene en °C y
 //la almacena en la unidad correcta
@@ -20,7 +22,7 @@ const setTemperature = (temp, unit) =>
 const getTime = (time) => {
   let temp = time.split(" ");
   temp = temp[1].split(":");
-  return temp[0];
+  return temp[0] >= 12 ? `${temp[0]}pm` : `${temp[0]}am`;
 };
 
 const giveMeUnit = () => {
@@ -31,23 +33,27 @@ const giveMeUnit = () => {
 const getIconURL = (iconName) =>
   ` http://openweathermap.org/img/wn/${iconName}@2x.png`;
 
-const converTemperature = (unit) => {
+const converTemperature = (myUnit) => {
   for (let i = 0; i < myList.length; i++) {
-    let {temp, unit: myUnit} = myList[i];
-    console.log(myList[i]);
-    myUnit = unit;
-    if (unit === "°F") {
+    let temp = myList[i].temp;
+    myList[i].unit = myUnit;
+    if (myUnit === "°F") {
       temp = ((temp * 9) / 5 + 32).toFixed();
     } else {
       temp = (((temp - 32) * 5) / 9).toFixed();
     }
+    myList[i].temp = temp;
   }
 };
 
 const createMyList = (list) => {
   myList = [];
   for (let i = 0; i < numOfReports; i++) {
-    const {dt_txt, main: {temp, humidity}, weather: [{main, icon}]} = list[i];
+    const {
+      dt_txt,
+      main: { temp, humidity },
+      weather: [{ main, icon }],
+    } = list[i];
     let tempOriginal = temp / 10;
     tempOriginal = tempOriginal.toFixed();
     let myTemperature = setTemperature(tempOriginal, giveMeUnit());
@@ -63,132 +69,80 @@ const createMyList = (list) => {
   }
 };
 
-// const createMyList = (list) => {
-//     myList = [];
-//     for (let i = 0; i < numOfReports; i++) {
-//       const element = list[i];
-//       let tempOriginal = element.main.temp / 10;
-//       tempOriginal = tempOriginal.toFixed();
-//       let myTemperature = setTemperature(tempOriginal, giveMeUnit());
-//       let forecast = {
-//         icon: getIconURL(element.weather[0].icon),
-//         temp: myTemperature,
-//         unit: giveMeUnit(),
-//         humi: element.main.humidity,
-//         main: element.weather[0].main,
-//         time: getTime(element.dt_txt),
-//       };
-//       myList.push(forecast);
-//     }
-//   };
-
-// const createMyNewList = (list) => {
-//   myList = [];
-//   const currentTime = today.getHours();
-//   for (let i = 0; i < numOfReports; i++) {
-//     const element = list[i];
-//     const elementTime = getTime(element.dt_txt);
-//     if (currentTime >= elementTime) {
-//       let tempOriginal = element.main.temp / 10;
-//       tempOriginal = tempOriginal.toFixed();
-//       let myTemperature = setTemperature(tempOriginal, giveMeUnit());
-//       let forecast = {
-//         icon: getIconURL(element.weather[0].icon),
-//         temp: myTemperature,
-//         unit: giveMeUnit(),
-//         humi: element.main.humidity,
-//         main: element.weather[0].main,
-//         time: getTime(element.dt_txt),
-//       };
-//       myList.push(forecast);
-//     }
-//   }
-// };
-const drawFirstElement = () =>{
-    console.log(myList[0]);
-    let firstElement = myList[0]; 
-    let mainReport = document.querySelector("#mainReport"); 
-    let mainImg = document.createElement("img");
-    mainImg.src = firstElement.icon;
-
-    let mainTemperature = document.createElement("h1");
-    mainTemperature.innerHTML = firstElement.temp;
-
-    let mainUnit = document.createElement("a");
-    mainUnit.innerHTML = firstElement.unit;
-
-    mainReport.appendChild(mainImg);
-    mainReport.appendChild(mainTemperature);
-    mainReport.appendChild(mainUnit);
-
-
-    cityNameDisplay.innerHTML = myCity;
-}
-
 const drawList = () => {
-  weatherDisplay.innerHTML = "";
-  cityNameDisplay.innerHTML = myCity;
-  drawFirstElement();
+  mainReport.innerHTML = "";
+  //Div para City
+  let cityNameDisplay = document.createElement("div");
+  cityNameDisplay.className = "main-city";
+  cityNameDisplay.innerHTML = `${myCity}, ${myCountry}`;
+  mainReport.appendChild(cityNameDisplay);
+  //Div para los partes
+  let forecastList = document.createElement("div");
+  forecastList.className = "forecast-list";
+  mainReport.appendChild(forecastList);
+  //Agregar Partes
   for (let i = 0; i < myList.length; i++) {
-    const {temp, unit, humi, main, time, icon: iconSrc} = myList[i];
-    let ulElement = document.createElement("ul");
-    let icon = document.createElement("img");
-    let liTemp = document.createElement("li");
-    let liHum = document.createElement("li");
-    let liMain = document.createElement("li");
-    let liTime = document.createElement("li");
-    icon.src = iconSrc;
-    liTemp.innerHTML = `Temp: ${temp}${unit}`;
-    liHum.innerHTML = `Hum: ${humi}%`;
-    liMain.innerHTML = main;
-    liTime.innerHTML = time;
-    ulElement.appendChild(icon);
-    ulElement.appendChild(liTemp);
-    ulElement.appendChild(liHum);
-    ulElement.appendChild(liMain);
-    ulElement.appendChild(liTime);
-    weatherDisplay.appendChild(ulElement);
-  }
-};
+    const { temp, unit, humi, time, icon: iconSrc } = myList[i];
 
-const createReportHTML = () => {
-  weatherDisplay.innerHTML = "";
-  drawFirstElement();  
-  for (let i = 0; i < myList.length; i++) {
-    const element = myList[i];
-    let ulElement = document.createElement("ul");
-    let icon = document.createElement("img");
-    let liTemp = document.createElement("li");
-    let liHum = document.createElement("li");
-    let liMain = document.createElement("li");
-    let liTime = document.createElement("li");
-    icon.src = element.icon;
-    liTemp.innerHTML = `Temp: ${element.temp}${element.unit}`;
-    liHum.innerHTML = `Hum: ${element.humi}%`;
-    liMain.innerHTML = element.main;
-    liTime.innerHTML = element.time;
-    ulElement.appendChild(icon);
-    ulElement.appendChild(liTemp);
-    ulElement.appendChild(liHum);
-    ulElement.appendChild(liMain);
-    ulElement.appendChild(liTime);
-    weatherDisplay.appendChild(ulElement);
+    let mainTime = document.createElement("div");
+    mainTime.className = "main-time";
+    mainTime.innerHTML = time;
+
+    let mainImg = document.createElement("img");
+    mainImg.src = iconSrc;
+
+    let mainVariables = document.createElement("div");
+    mainVariables.className = "main-variables";
+    let layoutTemperature = document.createElement("div");
+    layoutTemperature.className = "layout-temperature";
+    let layoutHumidity = document.createElement("div");
+
+    let mainTemperature = document.createElement("div");
+    mainTemperature.innerHTML = temp;
+    mainTemperature.className = "main-temperature";
+
+    let mainUnit = document.createElement("div");
+    mainUnit.innerHTML = unit;
+    mainUnit.className = "main-unit";
+
+    layoutTemperature.appendChild(mainTemperature);
+    layoutTemperature.appendChild(mainUnit);
+
+    let mainHumidity = document.createElement("div");
+    mainHumidity.innerHTML = `${humi}%`;
+    mainHumidity.className = "main-humidity";
+    layoutHumidity.appendChild(mainHumidity);
+    mainVariables.appendChild(layoutTemperature);
+    mainVariables.appendChild(layoutHumidity);
+
+    let singleFullReport = document.createElement("div");
+    singleFullReport.className = "single-full-report";
+    let singleReportInfo = document.createElement("div");
+    singleReportInfo.className = "single-report";
+    singleReportInfo.appendChild(mainImg);
+    singleReportInfo.appendChild(mainVariables);
+    singleFullReport.appendChild(mainTime);
+    singleFullReport.appendChild(singleReportInfo);
+    forecastList.appendChild(singleFullReport);
   }
 };
 
 const handleClick = (myRadio) => {
   myRadio.checked = true;
   if (myRadio.value !== myList[0].unit) {
+    console.log(myRadio.value);
+    myRadio.value === "°C"
+      ? (radioBfahrenheit.checked = false)
+      : (radioBCelsius.checked = false);
     converTemperature(myRadio.value);
   }
   drawList();
 };
 
 const getInfoAPI = (cityName, keyAPI) => {
-  //   let promise = fetch(
-  //     `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${keyAPI}`
-  //   );
-  let promise = fetch("weather-app.json");
+  let promise = fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${keyAPI}`
+  );
   promise
     .then(function (response) {
       if (response.status !== 200) {
@@ -199,9 +153,14 @@ const getInfoAPI = (cityName, keyAPI) => {
       }
       //   // Examine the text in the response
       response.json().then(function (data) {
-        myCity = data.city.name;
+        radioBCelsius.checked = true;
+        radioBfahrenheit.checked = false;
+        let {
+          city: { name, country },
+        } = data;
+        myCity = name;
+        myCountry = country;
         createMyList(data.list);
-        console.log(data.list[0]);
         drawList();
       });
     })
@@ -211,23 +170,13 @@ const getInfoAPI = (cityName, keyAPI) => {
 };
 
 getInfoAPI("San Diego", key);
-
-radioBCelsius.addEventListener("change", (evento) => {
-  radioBfahrenheit.checked = false;
-  let { target } = evento;
-  handleClick(target);
-});
-radioBfahrenheit.addEventListener("change", (evento) => {
-  radioBCelsius.checked = false;
-  let target = evento.target;
-  handleClick(target, radioBCelsius);
-});
-
 submitButton.addEventListener("click", (event) => {
   weatherDisplay.innerHTML = "";
   let cityName = textBoxCity.value;
   cityName !== "" ? getInfoAPI(cityName, key) : alert("enter a city");
-
-  //console.log(cityName);
 });
 
+unitDIV.addEventListener("click", (event) => {
+  let { target } = event;
+  if (target.type == "radio") handleClick(target);
+});
